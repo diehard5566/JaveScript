1a5e52fda5102f699d0ef2a512e84f19f2cbbefc
diehard5566 <shihyao001@gmail.com>
2022-03-15 01:45:47 +0800

    0314

--- Edabit/20220314/5.Ones,Threes-and-Nines.js
+++ Edabit/20220314/5.Ones,Threes-and-Nines.js
@@ -2,36 +2,36 @@
 // You must create a class. You will make variables (class.ones, class.threes, class.nines) to do this.
 
 
-// class OnesThreesNines {
-//     constructor(n) {
-//         this.ones = Math.floor(n / 1);
-//         this.threes = Math.floor(n / 3);
-//         this.nines = Math.floor(n / 9);
-//     }
-// }
+class OnesThreesNines {
+    constructor(n) {
+        this.ones = Math.floor(n / 1);
+        this.threes = Math.floor(n / 3);
+        this.nines = Math.floor(n / 9);
+    }
+}
 
-// class OnesThreesNines {
-//     constructor(num) {
-//         this.num = num;
-//     }
-//     get ones() {
-//         return this.num / 1
-//     }
-//     get threes() {
-//         return Math.floor(this.num / 3)
-//     }
-//     get nines() {
-//         return Math.floor(this.num / 9)
-//     }
-// }
+class OnesThreesNines {
+    constructor(num) {
+        this.num = num;
+    }
+    get ones() {
+        return this.num / 1
+    }
+    get threes() {
+        return Math.floor(this.num / 3)
+    }
+    get nines() {
+        return Math.floor(this.num / 9)
+    }
+}
 
 
 
-// let n1 = new OnesThreesNines(5)
-// console.log(n1.nines);  // 0
+let n1 = new OnesThreesNines(5)
+console.log(n1.nines);  // 0
 
-// console.log(n1.ones)// 5
+console.log(n1.ones)// 5
 
-// console.log(n1.threes) // 1
+console.log(n1.threes) // 1
 
 
